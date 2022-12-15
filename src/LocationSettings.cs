b117a53aa220f77715a9d272a03e4e1f29bfb3ca
Vidar Kongsli <vidar@kongs.li>
2022-12-15 06:57:01 +0100

    Validation

--- src/LocationSettings.cs
+++ src/LocationSettings.cs
@@ -1,6 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace src;
 
 public class LocationSettings
 {
+    [Required]
+    [StringLength(19, MinimumLength = 19, ErrorMessage = "Coordinates are not valid")]
     public string? Coordinates { get; set; }
 }
--- src/Program.cs
+++ src/Program.cs
@@ -7,7 +7,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddOptions<LocationSettings>()
-    .Bind(builder.Configuration.GetSection(nameof(LocationSettings)));
+    .Bind(builder.Configuration.GetSection(nameof(LocationSettings)))
+    .ValidateDataAnnotations();
 
 var app = builder.Build();
 
