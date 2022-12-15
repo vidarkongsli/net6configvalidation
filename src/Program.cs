c623adab8de27b37c559593e926d56248f1f4aa8
Vidar Kongsli <vidar@kongs.li>
2022-12-15 06:57:52 +0100

    Validate on start

--- src/Program.cs
+++ src/Program.cs
@@ -8,7 +8,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddOptions<LocationSettings>()
     .Bind(builder.Configuration.GetSection(nameof(LocationSettings)))
-    .ValidateDataAnnotations();
+    .ValidateDataAnnotations()
+    .ValidateOnStart();
 
 var app = builder.Build();
 
