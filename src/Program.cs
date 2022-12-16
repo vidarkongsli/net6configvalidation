6c80aa6530602735bea0c54027c3973df922a53b
Vidar Kongsli <vidar@kongs.li>
2022-12-16 10:41:26 +0100

    Bearer validation

--- src/Program.cs
+++ src/Program.cs
@@ -1,8 +1,6 @@
 using Microsoft.ApplicationInsights;
-using Microsoft.ApplicationInsights.Extensibility;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Extensions.Options;
-using Microsoft.IdentityModel.Tokens;
 using src;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -14,11 +12,19 @@ services.AddOptions<LocationSettings>()
     .Bind(builder.Configuration.GetSection(nameof(LocationSettings)))
     .ValidateDataAnnotations()
     .ValidateOnStart();
-    
+
+services.AddOptions<JwtBearerOptions>()
+    .Bind(builder.Configuration.GetSection(nameof(JwtBearerOptions)))
+    .Validate(options =>
+    {
+        return options.Authority != null;
+    })
+    .ValidateOnStart();
+
 services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddJwtBearer(options =>
     {
-        builder.Configuration.Bind(options.GetType().Name, options);   
+        builder.Configuration.Bind(options.GetType().Name, options);
     });
 
 var app = builder.Build();
@@ -38,6 +44,6 @@ catch (OptionsValidationException e)
 {
     var sp = builder.Services.BuildServiceProvider();
     var telemetryClient = sp.GetRequiredService<TelemetryClient>();
-    telemetryClient.TrackException(e);
+    telemetryClient.TrackException(e, new Dictionary<string, string> { { "OptionsType", e.OptionsType.FullName ?? string.Empty } });
     telemetryClient.Flush();
 }
